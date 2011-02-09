56e18e1ee53006675bd167d1a6956ba303738921
chaostone <duantihua@gmail.com>
2011-02-09 14:58:11 +0800

    remove packagekit subproject

--- packagekit/org.beangle.packagekit/src/main/java/org/beangle/packagekit/engine/TaskStatus.java
+++
@@ -1,9 +0,0 @@
-/* Copyright c 2005-2012.
- * Licensed under GNU  LESSER General Public License, Version 3.
- * http://www.gnu.org/licenses
- */
-package org.beangle.packagekit.engine;
-
-public enum TaskStatus {
-	WAIT, START, END
-}
