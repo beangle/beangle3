47b0368b0ea8f88d089815f57cba14c75b50f661
Daniel Qian <chanjarster@gmail.com>
2012-02-16 16:23:24 +0800

    add Spring test case infras

---
+++ library/org.beangle.test/src/main/java/org/beangle/test/spring/SpringTestBootstrap.java
@@ -0,0 +1,30 @@
+/* Copyright c 2005-2012.
+ * Licensed under GNU  LESSER General Public License, Version 3.
+ * http://www.gnu.org/licenses
+ */
+package org.beangle.test.spring;
+
+import org.beangle.model.persist.EntityDao;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+
+/**
+ * 基础服务的测试用例
+ * 
+ * @author chaostone 2005-9-8
+ */
+@ContextConfiguration("classpath:applicationContext-test.xml")
+public class SpringTestBootstrap extends AbstractTestNGSpringContextTests {
+
+	public EntityDao entityDao;
+
+	public EntityDao getEntityDao() {
+		return entityDao;
+	}
+
+	public void setEntityDao(EntityDao entityDao) {
+		this.entityDao = entityDao;
+	}
+
+}
