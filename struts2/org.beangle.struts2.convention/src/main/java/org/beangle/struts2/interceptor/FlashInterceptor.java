2438a48abbd6f0666eb1847362970e2b534bc830
duantihua <duantihua@gmail.com>
2012-11-22 00:57:46 +0800

    update new license header

--- struts2/org.beangle.struts2.convention/src/main/java/org/beangle/struts2/interceptor/FlashInterceptor.java
+++ struts2/org.beangle.struts2.convention/src/main/java/org/beangle/struts2/interceptor/FlashInterceptor.java
@@ -1,6 +1,20 @@
-/* Copyright c 2005-2012.
- * Licensed under GNU  LESSER General Public License, Version 3.
- * http://www.gnu.org/licenses
+/*
+ * Beangle, Agile Java/Scala Development Scaffold and Toolkit
+ *
+ * Copyright (c) 2005-2012, Beangle Software.
+ *
+ * Beangle is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Beangle is distributed in the hope that it will be useful.
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Beangle.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.beangle.struts2.interceptor;
 
