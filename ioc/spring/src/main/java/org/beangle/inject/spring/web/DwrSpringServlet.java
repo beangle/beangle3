3a83c2a9e5e1cce104079e414b17ca9a640380fa
chaostone <duantihua@gmail.com>
2016-04-01 10:25:35 +0800

    update license header

--- ioc/spring/src/main/java/org/beangle/inject/spring/web/DwrSpringServlet.java
+++ ioc/spring/src/main/java/org/beangle/inject/spring/web/DwrSpringServlet.java
@@ -1,20 +1,20 @@
 /*
- * OpenURP, Agile Development Scaffold and Toolkit
+ * Beangle, Agile Development Scaffold and Toolkit
  *
- * Copyright (c) 2005-2015, OpenURP Software.
+ * Copyright (c) 2005-2016, Beangle Software.
  *
- * OpenURP is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * Beangle is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * OpenURP is distributed in the hope that it will be useful.
+ * Beangle is distributed in the hope that it will be useful.
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with OpenURP.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Beangle.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.beangle.inject.spring.web;
 
